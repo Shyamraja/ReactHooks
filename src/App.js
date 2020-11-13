c37a45879e262ceb37618f7c391d0d901f95d6a9
Shyamraja <t7bhsh00@students.oamk.fi>
2020-11-13 23:50:58 +0200

    Learning useState hooks with Profile and counter components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-
 import Profile from './component/Profile'
-
+import Counter from './component/Counter'
 function App() {
   return (
     <div className="App">
       <Profile /> 
+      <div className="Counter">
+        <Counter/>
+      </div>
     </div>
   );
 }
